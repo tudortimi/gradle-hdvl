71644fcf05ac0ca0a594752ac17b702513b35f78
Tudor Timi <tudortimi@users.noreply.github.com>
2020-12-27 15:38:13 +0100

    Access 'sourceSets.main' using DSL
    This still requires an extra '.get()' call. See the comment.

--- src/functTest/groovy/com/verificationgentleman/gradle/hdvl/SystemVerilogPluginTest.groovy
+++ src/functTest/groovy/com/verificationgentleman/gradle/hdvl/SystemVerilogPluginTest.groovy
@@ -200,9 +200,10 @@ class SystemVerilogPluginFunctionalTest extends Specification {
             }
             
             tasks.register<Copy>("copy") {
-                // XXX Not clear why we can't just do 'sourceSets.main.sv'. This is probably because when the extension
-                // is added, the element type of 'sourceSets' is lost.
-                from(sourceSets.get("main").sv.files)
+                // XXX Not clear why we can't just do 'sourceSets.main.sv'.
+                // 'sourceSets.main' doesn't return an object of type 'SourceSet', but a
+                // 'NamedDomainObjectProvider<SourceSet'. The Java plugin has the same issue.
+                from(sourceSets.main.get().sv.files)
                 into("build")
             }
         """
