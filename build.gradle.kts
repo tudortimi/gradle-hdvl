ea40365ac935df4aa5670a3b02b4b85f55f4a345
Tudor Timi <tudortimi@users.noreply.github.com>
2020-12-28 14:47:02 +0100

    Add Maven publishing
    This allows the plugin to be published to 'MavenLocal' and to be picked
    up by other projects from there.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     id("java-gradle-plugin")
     id("groovy")
+    id("maven-publish")
 }
 
 apply(from = "$rootDir/gradle/functional-test.gradle")
+
+group = "com.verificationgentleman.gradle"
+version = "0.1.0-SNAPSHOT"
 
 gradlePlugin {
     plugins {
