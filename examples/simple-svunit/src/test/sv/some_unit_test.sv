2f1acf4aabecd6a3a6652e8428f6d6ac45dba566
Tudor Timi <tudortimi@users.noreply.github.com>
2021-01-09 11:25:20 +0100

    Update 'simple-svunit' example to be a full fledged setup
    It now contains some dummy production code and a test that really runs.

--- examples/simple-svunit/src/test/sv/some_unit_test.sv
+++ examples/simple-svunit/src/test/sv/some_unit_test.sv
@@ -1,2 +1,35 @@
-module some_unit_test;
+module some_class_unit_test;
+
+  import svunit_pkg::svunit_testcase;
+  `include "svunit_defines.svh"
+
+  string name = "some_class_unit_test";
+  svunit_testcase svunit_ut;
+
+
+  import some_package::*;
+
+
+  function void build();
+    svunit_ut = new(name);
+  endfunction
+
+  task setup();
+    svunit_ut.setup();
+  endtask
+
+  task teardown();
+    svunit_ut.teardown();
+  endtask
+
+
+  `SVUNIT_TESTS_BEGIN
+
+    `SVTEST(is_working__returns_1)
+      some_class obj = new();
+      `FAIL_UNLESS(obj.is_working())
+    `SVTEST_END
+
+  `SVUNIT_TESTS_END
+
 endmodule
