3229a01e0006f4daabb0eb122564190e9c54eae9
Tudor Timi <tudortimi@users.noreply.github.com>
2022-02-27 14:42:35 -0800

    Add empty line to fix style

--- src/main/java/com/verificationgentleman/gradle/hdvl/c/CPlugin.java
+++ src/main/java/com/verificationgentleman/gradle/hdvl/c/CPlugin.java
@@ -25,6 +25,7 @@ import org.gradle.api.Project;
 import org.gradle.api.internal.plugins.DslObject;
 
 public class CPlugin implements Plugin<Project> {
+
     @Override
     public void apply(Project project) {
         project.getPluginManager().apply(HDVLBasePlugin.class);
