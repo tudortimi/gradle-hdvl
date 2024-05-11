cb4f65299d2c0706c9d1297bd4c3abc596a7a759
Tudor Timi <tudortimi@users.noreply.github.com>
2024-05-12 00:11:03 +0300

    Simplify conversion of compile spec to XML using JAXB
    The Java Architecture for XML Binding (JAXB) simplifies the code a lot. It will also make it easier to perform the reverse operation.
    
    The tests fail at the moment, because the XMLs that are produced contain absolute paths for the files.

--- src/main/java/com/verificationgentleman/gradle/hdvl/internal/DefaultHDVLCompileSpec.java
+++ src/main/java/com/verificationgentleman/gradle/hdvl/internal/DefaultHDVLCompileSpec.java
@@ -2,12 +2,18 @@ package com.verificationgentleman.gradle.hdvl.internal;
 
 import com.verificationgentleman.gradle.hdvl.HDVLCompileSpec;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.File;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+@XmlRootElement(name="compileSpec")
 public class DefaultHDVLCompileSpec implements HDVLCompileSpec {
+    @XmlElementWrapper
+    @XmlElement(name="svSourceFile")
     private final File[] svSourceFiles;
 
     public DefaultHDVLCompileSpec(File[] svSourceFiles) {
@@ -16,6 +22,12 @@ public class DefaultHDVLCompileSpec implements HDVLCompileSpec {
 
     public DefaultHDVLCompileSpec(Set<File> svSourceFiles) {
         this.svSourceFiles = svSourceFiles.toArray(new File[0]);
+    }
+
+    // Needed for JAXB
+    @SuppressWarnings("unused")
+    private DefaultHDVLCompileSpec() {
+        this(new File[0]);
     }
 
     @Override
--- src/main/java/com/verificationgentleman/gradle/hdvl/internal/WriteCompileSpecFile.java
+++ src/main/java/com/verificationgentleman/gradle/hdvl/internal/WriteCompileSpecFile.java
@@ -1,23 +1,13 @@
 package com.verificationgentleman.gradle.hdvl.internal;
 
-import com.verificationgentleman.gradle.hdvl.HDVLCompileSpec;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 public class WriteCompileSpecFile extends DefaultTask {
     private final RegularFileProperty destination;
@@ -44,45 +34,13 @@ public class WriteCompileSpecFile extends DefaultTask {
     @TaskAction
     protected void generate() {
         DefaultHDVLCompileSpec compileSpec = new DefaultHDVLCompileSpec(getSvSource().getFiles());
-        Document document = getDocument(compileSpec);
-        writeFile(document);
-    }
-
-    private Document getDocument(HDVLCompileSpec compileSpec) {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.newDocument();
+            JAXBContext jaxbContext = JAXBContext.newInstance(DefaultHDVLCompileSpec.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-            Element rootElement = document.createElement("compileSpec");
-            document.appendChild(rootElement);
-
-            Element svSourceFilesElement = document.createElement("svSourceFiles");
-            rootElement.appendChild(svSourceFilesElement);
-
-            for (File svSource : compileSpec.getSvSourceFiles()) {
-                Element svSourceFileElement = document.createElement("svSourceFile");
-                svSourceFileElement.appendChild(document.createTextNode(getProject().relativePath(svSource)));
-                svSourceFilesElement.appendChild(svSourceFileElement);
-            }
-
-            return document;
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void writeFile(Document document) {
-        try {
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(destination.get().getAsFile());
-            transformer.transform(source, result);
-        } catch (TransformerException e) {
+            marshaller.marshal(compileSpec, destination.get().getAsFile());
+        } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
     }
