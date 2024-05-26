21dca8ade969ecb9b52d427af5bc0e5a37a6aed0
Tudor Timi <tudortimi@users.noreply.github.com>
2024-05-26 17:55:44 +0200

    Add exported header to example using published dependency

--- examples/using-published/some-project/src/main/sv/some_project.sv
+++ examples/using-published/some-project/src/main/sv/some_project.sv
@@ -1,10 +1,12 @@
 module some_project;
     import some_published_dependency::*;
+    `include "some_published_dependency_macros.svh"
 
     initial
         do_stuff();
 
     function automatic void do_stuff();
         some_class o = new();
+        `some_published_dependency_macro
     endfunction
 endmodule
